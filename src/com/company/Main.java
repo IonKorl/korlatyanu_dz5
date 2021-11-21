2403e6477c6b5aa063b5e7e30ee728a4bd3d8858
IonKorl <ikorlatyanu@mail.ru>
2021-11-21 12:25:19 +0500

    korlatyanu_homework05

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        while (a != -1) {
+            int minDigit = 9;
+            int firstDigit = a / 100;
+            int promezhutochnoe = a / 10;
+            int secondDigit = promezhutochnoe % 10;
+            int thirdDigit = a % 10;
+            if (firstDigit < minDigit){
+                minDigit = firstDigit;
+            }
+            if (secondDigit < minDigit) {
+                minDigit = firstDigit;
+            }
+            if (thirdDigit < minDigit) {
+                minDigit = firstDigit;
+            }
+            a = scanner.nextInt();
+
+        }
+        System.out.println("Минимальная цифра из веденных данных равна" + minDigit);
+
+    }
+}
